acba7f829b1f5fd82c4de00909f29f7b231a48f6
Thierry Lelegard <thierry@lelegard.fr>
2023-10-22 13:47:37 +0200

    Added set-git-putty-pageant

---
+++ set-git-putty-pageant.ps1
@@ -0,0 +1,18 @@
+﻿#-----------------------------------------------------------------------------
+#
+#  Copyright (c) 2022, Thierry Lelegard
+#  BSD-2-Clause license, see LICENSE.txt file
+#
+#  Define environment variable so that git uses Putty Pageant as SSH agent.
+#
+#-----------------------------------------------------------------------------
+
+[CmdletBinding(SupportsShouldProcess=$true)]
+param([switch]$NoPause = $false)
+
+Write-Output "==== Git will now use Putty Pageant as SSH agent"
+
+. "$PSScriptRoot\install-common.ps1"
+Define-UserEnvironment "GIT_SSH" "C:\Program Files\PuTTY\plink.exe"
+Send-SettingChange
+Exit-Script
