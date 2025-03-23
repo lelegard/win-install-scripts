2ec053be2ec86015377a1fff4095d930e94cf5f6
Thierry Lelegard <thierry@lelegard.fr>
2025-03-23 22:14:21 +0100

    Update install-winget

--- install-winget.ps1
+++ install-winget.ps1
@@ -42,18 +42,28 @@ if (-not (Find-WinGet)) {
         # See https://learn.microsoft.com/en-us/windows/package-manager/winget/
         # We noticed intermittent failures, retry up to 3 times.
         $retries = 3
+        Write-Output "PowerShell version: $($PSVersionTable.PSVersion.ToString())"
         while (-not (Find-WinGet) -and $retries -gt 0) {
             $retries--
-            Write-Output "PowerShell version: $($PSVersionTable.PSVersion.ToString())"
             if ($PSVersionTable.PSVersion.Major -lt 7) {
                 Write-Output "Installing NuGet ..."
                 Install-PackageProvider -Name "NuGet" -Force -ForceBootstrap -ErrorAction Continue
             }
-            Write-Output "Installing Microsoft.WinGet.Client PowerShell module ..."
-            Install-Module -Name Microsoft.WinGet.Client -AcceptLicense -Force -AllowClobber -Repository PSGallery -ErrorAction Continue
+            if (-not (Get-Module -Name Microsoft.WinGet.Client -ListAvailable)) {
+                Write-Output "Installing Microsoft.WinGet.Client PowerShell module ..."
+                Install-Module -Name Microsoft.WinGet.Client -AcceptLicense -Force -AllowClobber -Repository PSGallery -ErrorAction Continue
+            }
             Import-Module -Name Microsoft.WinGet.Client
             Write-Output "Installing WinGet ..."
             try { Repair-WinGetPackageManager -AllUsers } catch {}
+            # We noticed some spurious asynchronous activity after installation.
+            # Let this background activity complete.
+            $timeout = 5
+            while (-not (Find-WinGet) -and $timeout -gt 0) {
+                $timeout--
+                Write-Output "WinGet not found, retrying in two seconds..."
+                Start-Sleep -Seconds 2
+            }
         }
     }
 }
